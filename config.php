99a74c81f358cf1f6a013f667815f2f93eb4b997
Anh Pham <vinhanh@gmail.com>
2012-09-07 18:16:15 -0400

    update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 	define(DB_PASSWORD, "password");
 	define(DB_HOST, "localhost");
 	define(DB_NAME, "stage_siloz");
-	define(ABS_PATH, "/var/www/vhosts/siloz.com/anh/website");
-	define(API_URL, "http://anh.siloz.com/website/api.php");
-	define(ACTIVE_URL, "http://anh.siloz.com/website");
+	define(ABS_PATH, "/var/www/vhosts/stage.anh.siloz.com/httpdocs/website");
+	define(API_URL, "http://stage.anh.siloz.com/website/api.php");
+	define(ACTIVE_URL, "http://stage.anh.siloz.com/website");
 	define(FACEBOOK_ID, "164873893645808");
 	define(OPENINVITER_KEY, "848c26c450b38b89fc9de61013811701");
 ?>
