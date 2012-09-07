c84cf7bb6714479cf9a3b063df2e2cfedf9f0551
Anh Pham <vinhanh@gmail.com>
2012-09-07 17:27:09 -0400

    update config file for developers

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-	define(DB_USERNAME, "siloz_admin");
-	define(DB_PASSWORD, "P@ssword2010");
+	define(DB_USERNAME, "siloz_dev");
+	define(DB_PASSWORD, "password");
 	define(DB_HOST, "localhost");
-	define(DB_NAME, "siloz");
-	define(ABS_PATH, "/var/www/vhosts/siloz.com/launch");
-	define(API_URL, "http://launch.siloz.com/api.php");
-	define(ACTIVE_URL, "http://launch.siloz.com");
+	define(DB_NAME, "stage_siloz");
+	define(ABS_PATH, "/var/www/vhosts/siloz.com/anh/website");
+	define(API_URL, "http://anh.siloz.com/website/api.php");
+	define(ACTIVE_URL, "http://anh.siloz.com/website");
 	define(FACEBOOK_ID, "164873893645808");
 	define(OPENINVITER_KEY, "848c26c450b38b89fc9de61013811701");
 ?>
