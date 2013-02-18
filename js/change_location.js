90ca210c0f5c778d45d1fdc2d6333ff2a8d5d85e
Bo Chen <bochen303@gmail.com>
2013-02-17 19:52:48 -0800

    fixing change location styling

--- js/change_location.js
+++ js/change_location.js
@@ -4,7 +4,7 @@ $(function() {
 
 function initializeChangeLocationElements() {
 	$('.change_location').click(function() {
-		var str = '<form action="" method="POST"><input onclick=this.value=""; type="text" value="Enter Zip Code" name="zip"> <button type="submit" name="location" value="Update">Update</button></form>';
+		var str = '<form action="" method="POST" style="display: inline-block;"><input onclick=this.value=""; type="text" value="Enter Zip Code" name="zip"> <button type="submit" name="location" value="Update">Update</button></form>';
 		$(this).replaceWith( str );
 	});
 }
